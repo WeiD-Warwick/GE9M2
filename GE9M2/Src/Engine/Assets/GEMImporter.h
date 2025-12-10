77582ed4b3eebd29a3e92447c740c51e2140eb3f
WeiD-Warwick <u5726890@live.warwick.ac.uk>
2025-12-10 18:25:29 +0000

    update

--- GE9M2/Src/Engine/Assets/GEMImporter.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../../../Third_Party/GEMLoader.h"
-
-class GEMImporter {
-public:
-    static bool loadStaticMesh(const std::string& path, MeshData& mesh);
-    static bool loadAnimatedMesh(const std::string& path, MeshData& mesh, Skeleton& sk, std::vector<AnimationClip>& anims);
-};
